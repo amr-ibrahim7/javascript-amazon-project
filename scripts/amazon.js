6458ca87b53dd8fbb108248492d303fd81ed0626
amr-ibrahim7 <amrnew100@gmail.com>
2024-08-06 17:59:13 +0300

    Create automated tests for addToCart

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,7 +1,7 @@
 import { cart, addToCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 
-import { formatCurrency } from "./utilis/money.js";
+import { formatCurrency } from "./utils/money.js";
 
 let productsGrid = document.querySelector(".js-products-grid");
 
